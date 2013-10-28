11c8ad30d0a1555aa443a340ae40c40931ceb07f
Peter Binkley <peter.binkley@ualberta.ca>
2013-10-28 16:03:29 -0700

    forgot demo.sh

---
+++ demo.sh
@@ -0,0 +1,23 @@
+echo Create container
+
+curl -X PUT -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+
+echo List containers:
+
+curl -X GET -i -H "X-Auth-Token: $TOKEN" $URL
+
+echo Contents of container:
+
+curl -X GET -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+
+echo Metadata of container:
+
+curl -X HEAD -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+
+echo Put object:
+
+curl -X PUT -i -H "X-Auth-Token: $TOKEN" -T demo.sh $URL/testcontainer/testobject
+
+echo Retrieve object
+
+curl -X GET -i -H "X-Auth-Token: $TOKEN" -o retrieved_demo.sh $URL/testcontainer/testobject
