c976d1aae1b9a5de322a05e0ef163e87d87eae50
Timur Alperovich <timur@timuralp.com>
2019-06-14 16:39:01 -0700

    Use the git tags if possible, so we can tag Swift.
    It would be nice to tag the resulting images with the associated
    packaged Swift version.

--- publish_img.sh
+++ publish_img.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+export TAG=`git describe --all|cut -d / -f2-`
 docker tag $REPO:$COMMIT $REPO:$TAG
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+    docker tag $REPO:$COMMIT $REPO:latest
+fi
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
