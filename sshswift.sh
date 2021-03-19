14cf71abc334c785e86b3184f703c0f3986f70bf
Timur Alperovich <timur@timuralp.com>
2021-03-18 17:14:44 -0700

    Update the README with updated image pointers

--- sshswift.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# do not add Docker server to known_hosts, or next time auth will fail
-# use sed to extract port from " xxxxx->22 " 
-ssh -o "UserKnownHostsFile /dev/null" -p `sudo docker ps | grep docker-swift | sed 's/.* \([0-9]*\)->22.*/\1/'` swift@localhost
